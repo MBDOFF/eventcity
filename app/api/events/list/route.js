19b44e41435996d71e200f9a351caade63f455c9
Daniel <daniel@mbd.one>
2024-03-09 09:11:30 +0200

    Update route.js

--- app/api/events/list/route.js
+++ app/api/events/list/route.js
@@ -9,25 +9,3 @@ export async function GET() {
 
   return NextResponse.json(listPromise.documents);
 }
-
-/*export async function POST(req, res) {
-  const data = await req.json();
-  console.log(data);
-
-  if (!data.email || !data.pass) {
-    return NextResponse.json(
-      { error: "All fields are required" },
-      { status: 400 }
-    );
-  }
-
-  const verified = await verify(data.email, data.pass);
-
-  if(!verified.status){
-    return NextResponse.json({ error: verified.error }, { status: 400 });
-  }
-
-  const user = verified.user;
-  return NextResponse.json({ user });
-}
-*/
