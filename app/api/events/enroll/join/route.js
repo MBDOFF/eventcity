c7746f90fe0e83d254fbe0386a8c4eca0903201e
Daniel <daniel@mbd.one>
2024-03-09 12:52:35 +0200

    enroll

---
+++ app/api/events/enroll/join/route.js
@@ -0,0 +1,62 @@
+import { client, databases, ID, verify } from "@/files/js/db.js";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({});
+}
+
+export async function POST(req, res) {
+  const data = await req.json();
+  console.log(data);
+
+  if (!data.email || !data.pass || !data.event || !data.type) {
+    return NextResponse.json(
+      { error: "All fields are required" },
+      { status: 400 }
+    );
+  }
+
+  const verified = await verify(data.email, data.pass);
+
+  if (!verified.status) {
+    return NextResponse.json(verified, { status: 400 });
+  }
+
+  const user = verified.user;
+
+  const eventResponse = await databases.getDocument(
+    "65eba297f2b27e0ab9d0",
+    "65eba31400845ba99440",
+    data.event
+  );
+
+  if (!eventResponse) {
+    return NextResponse.json({ error: "Event not found" }, { status: 400 });
+  }
+
+  const event = eventResponse.document;
+  const users = event.users || [];
+  
+  if(data.type === "user"){
+    users.push({ user: user.$id, type: "user", status: "pending"});
+  }else if(data.type === "volunteer"){
+    if(!user.vol.desc){
+        return NextResponse.json(
+            { error: "You are not a volunteer" },
+            { status: 400 }
+        );
+    }
+    users.push({ user: user.$id, type: "volunteer", status: "pending"});
+  }
+
+  const response = await databases.updateDocument(
+    "65eba297f2b27e0ab9d0",
+    "65eba31400845ba99440",
+    data.event,
+    {
+        users: users
+    }
+  );
+
+  return NextResponse.json({ response });
+}
---
+++ app/api/events/enroll/leave/route.js
@@ -0,0 +1,57 @@
+import { client, databases, ID, verify } from "@/files/js/db.js";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({});
+}
+
+export async function POST(req, res) {
+  const data = await req.json();
+  console.log(data);
+
+  if (!data.email || !data.pass || !data.event) {
+    return NextResponse.json(
+      { error: "All fields are required" },
+      { status: 400 }
+    );
+  }
+
+  const verified = await verify(data.email, data.pass);
+
+  if (!verified.status) {
+    return NextResponse.json(verified, { status: 400 });
+  }
+
+  const user = verified.user;
+
+  const eventResponse = await databases.getDocument(
+    "65eba297f2b27e0ab9d0",
+    "65eba31400845ba99440",
+    data.event
+  );
+
+  if (!eventResponse) {
+    return NextResponse.json({ error: "Event not found" }, { status: 400 });
+  }
+
+  const event = eventResponse.document;
+  const users = event.users || [];
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].user === user.$id) {
+      users.splice(i, 1);
+      break;
+    }
+  }
+
+  const response = await databases.updateDocument(
+    "65eba297f2b27e0ab9d0",
+    "65eba31400845ba99440",
+    data.event,
+    {
+      users: users,
+    }
+  );
+
+  return NextResponse.json({ response });
+}
