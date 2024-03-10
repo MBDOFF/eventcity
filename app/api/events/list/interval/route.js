6ed25d1f81d22d308ff41f874ab4ecfbbb15b3d6
Daniel <daniel@mbd.one>
2024-03-10 08:27:10 +0200

    Update route.js

--- app/api/events/list/interval/route.js
+++ app/api/events/list/interval/route.js
@@ -30,13 +30,13 @@ export async function POST(req, res) {
   var _day = parseInt(_parts[0]);
   var _month = parseInt(_parts[1]);
   var _year = parseInt(_parts[2]);
-  var _targetDate = new Date(_year, _month - 1, _day);
+  var _targetDate = new Date(_year, _month, _day);
 
   var _parts2 = data.date2.split(".");
   var _day2 = parseInt(_parts2[0]);
   var _month2 = parseInt(_parts2[1]);
   var _year2 = parseInt(_parts2[2]);
-  var _targetDate2 = new Date(_year2, _month2 - 1, _day2);
+  var _targetDate2 = new Date(_year2, _month2, _day2);
 
 
   for (let i = 0; i < eventResponse.documents.length; i++) {
@@ -47,16 +47,16 @@ export async function POST(req, res) {
     var day1 = parseInt(parts1[0]);
     var month1 = parseInt(parts1[1]);
     var year1 = parseInt(parts1[2]);
-    var targetDate1 = new Date(year1, month1 - 1, day1);
+    var targetDate1 = new Date(year1, month1, day1);
 
     const isMulti = interval[1] !== "undefined";
-    var targetDate2 = new Date(year1, month1 - 1, day1);
+    var targetDate2 = new Date(year1, month1, day1);
     if (isMulti) {
       var parts2 = interval[1].split(".");
       var day2 = parseInt(parts2[0]);
       var month2 = parseInt(parts2[1]);
       var year2 = parseInt(parts2[2]);
-      targetDate2 = new Date(year2, month2 - 1, day2);
+      targetDate2 = new Date(year2, month2, day2);
     }
     
    // i want all the events that any of dates between _targetDate and _targetDate2 are between targetDate1 and targetDate2
