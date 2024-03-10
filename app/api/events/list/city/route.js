43e6a6d0f37f7fdec28438a87a41b620f24894b4
Daniel <daniel@mbd.one>
2024-03-10 08:12:24 +0200

    diacritice search

--- app/api/events/list/city/route.js
+++ app/api/events/list/city/route.js
@@ -26,5 +26,18 @@ export async function POST(req, res) {
     return NextResponse.json({ error: "Events not found" }, { status: 400 });
   }
 
-  return NextResponse.json(eventResponse.documents.filter(event => event.city.toLowerCase() === data.city.toLowerCase()));
+  function removeDiacritics(str) {
+    const diacriticsMap = {
+      'ă': 'a', 'â': 'a', 'î': 'i', 'ș': 's', 'ț': 't',
+      'Ă': 'A', 'Â': 'A', 'Î': 'I', 'Ș': 'S', 'Ț': 'T'
+    };
+
+    return str.replace(/[ăâîșțĂÂÎȘȚ]/g, match => diacriticsMap[match]);
+  }
+
+  const filteredEvents = eventResponse.documents.filter(event => {
+    return removeDiacritics(event.city.toLowerCase()) === removeDiacritics(data.city.toLowerCase());
+  });
+
+  return NextResponse.json(filteredEvents);
 }
